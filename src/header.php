e954d308bab32a31abbfbf8bdbcc19702f0390cd
Greg Comstock <gcomstock@gmail.com>
2016-06-20 23:38:27 -0700

    favicons

--- src/header.php
+++ src/header.php
@@ -33,7 +33,27 @@
   <meta name="description" content="Mark Riddick saw his first illustrations published in a small underground fanzine from Washington called, Scavenger Magazine. His first T-Shirt design, out of hundreds to come, was for Kentucky-based grindcore crust band, Son of Dog. After a few years publishing his work with bands and fanzines, Mark�s name soon became synonymous with underground death and black metal music." />
 	<meta name="keywords" content="riddickart, the art of mark riddick, mark riddick, fossil dungeon, illustrator, grotesque, filler art, underground, death/black metal, artwork, artist, fanzines, publications, musiq, design, logo, graphics, brutal, gore, hell, satan, athiest, heavy, morbid, profane, maggot, logos from hell" />
 
-  <link href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" rel="shortcut icon"/>
+  <!--FAVICONS-->
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-180x180.png">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-194x194.png" sizes="194x194">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/android-chrome-192x192.png" sizes="192x192">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="<?php bloginfo('template_directory'); ?>/images/favicons/manifest.json">
+  <link rel="mask-icon" href="<?php bloginfo('template_directory'); ?>/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="msapplication-TileImage" content="<?php bloginfo('template_directory'); ?>/images/favicons/mstile-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <!--/FAVICONS-->
 </head>
 <body>
   <?php include(TEMPLATEPATH.'/warning.php'); ?>
