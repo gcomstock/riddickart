1d0b937e5f6729e480f8c925d7c5d2b63cebc070
Greg Comstock <gcomstock@gmail.com>
2016-06-21 08:09:41 -0700

    gallery lazy loader

--- src/gallery.php
+++ src/gallery.php
@@ -9,31 +9,61 @@ Template Name: Instagram Gallery
 
 <div class="R__main">
 
-  <div id="R__main__gallery">
-  </div>
+  <div id="R__main__gallery"></div>
 
   <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/instafeed.min.js"></script>
 
   <script type="text/javascript">
+    /*
+      CLIENT INFO
+      CLIENT ID	8d5a6df20c714cbaa7a027fafbade629
+      WEBSITE URL	riddickart.com
+      REDIRECT URI	http://www.riddickart.com
+      SUPPORT EMAIL	gcomstock@gmail.com
+      userID: 2344925701
+    */
+
     window.onload = function(){
 
-    var template = '<a href="{{link}}" class="R__main__gallery__img" style="background-image:url({{image}})" target="_blank">'+
-                   '  <div class="R__main__gallery__img__caption">{{caption}}</div>'+
-                   '</a>'
+      var shouldFetch = true;
+      var gallery = document.getElementById('R__main__gallery');
+      var template = '<a href="{{link}}" class="R__main__gallery__img" style="background-image:url({{image}})" target="_blank">'+
+                     '  <div class="R__main__gallery__img__caption">{{caption}}</div>'+
+                     '</a>';
+
+      function fetchMoreImages(){
+        if (shouldFetch && window.scrollY > (gallery.offsetHeight - window.outerHeight) ) {
+          shouldFetch = false;
+          feed.next();
+        }
+      }
 
       var feed = new Instafeed({
         get: 'user',
-        limit: 18,
+        limit: 6,
         target: 'R__main__gallery',
         userId: 178110804,
         clientId: '54db78126f764a879ba681b4a9db8140',
         accessToken: '178110804.54db781.e9695f2f1ddf4ce6b68cf91798e131e9',
         template: template,
-        resolution: 'low_resolution'
+        resolution: 'low_resolution',
+        after: function() {
+          var tns = document.querySelectorAll('.R__main__gallery__img');
+
+          for (var i = 0; i < tns.length; i++) {
+            tns[i].offsetHeight; //force reflow
+            tns[i].style.opacity = 1;
+            tns[i].style.transform = 'scale3d(1,1,1)';
+          };
+
+          if (this.hasNext()) {
+            shouldFetch = true;
+          }
+        }
       });
 
       feed.run();
-
+      window.addEventListener('scroll', fetchMoreImages);
     };
   </script>
 
--- src/header.php
+++ src/header.php
@@ -5,59 +5,58 @@
  */
 ?>
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 
 <head profile="http://gmpg.org/xfn/11">
-
   <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css" type="text/css" media="screen" />
 
-	<!-- begin wordpress default header strings, do not modify -->
-	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+	<!--WORDPRESS HEADER-->
+    <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
 
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />	
-	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats please -->
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats please -->
 
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css" type="text/css" media="screen" />
+    <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+    <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+    <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
-	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	
-	<?php wp_get_archives('type=monthly&format=link'); ?>
-	<?php //comments_popup_script(); // off by default ?>
-	<?php wp_head(); ?>
-	<!-- end wordpress default header strings -->
-    
-  <meta name="description" content="Mark Riddick saw his first illustrations published in a small underground fanzine from Washington called, Scavenger Magazine. His first T-Shirt design, out of hundreds to come, was for Kentucky-based grindcore crust band, Son of Dog. After a few years publishing his work with bands and fanzines, Mark�s name soon became synonymous with underground death and black metal music." />
-	<meta name="keywords" content="riddickart, the art of mark riddick, mark riddick, fossil dungeon, illustrator, grotesque, filler art, underground, death/black metal, artwork, artist, fanzines, publications, musiq, design, logo, graphics, brutal, gore, hell, satan, athiest, heavy, morbid, profane, maggot, logos from hell" />
+    <?php wp_get_archives('type=monthly&format=link'); ?>
+    <?php //comments_popup_script(); // off by default ?>
+    <?php wp_head(); ?>
+	<!--/WORDPRESS HEADER-->
+
+  <!--SEO-->
+    <meta name="description" content="Mark Riddick saw his first illustrations published in a small underground fanzine from Washington called, Scavenger Magazine. His first T-Shirt design, out of hundreds to come, was for Kentucky-based grindcore crust band, Son of Dog. After a few years publishing his work with bands and fanzines, Mark�s name soon became synonymous with underground death and black metal music." />
+	  <meta name="keywords" content="riddickart, the art of mark riddick, mark riddick, fossil dungeon, illustrator, grotesque, filler art, underground, death/black metal, artwork, artist, fanzines, publications, musiq, design, logo, graphics, brutal, gore, hell, satan, athiest, heavy, morbid, profane, maggot, logos from hell" />
+  <!--SEO-->
 
   <!--FAVICONS-->
-  <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-180x180.png">
-  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-194x194.png" sizes="194x194">
-  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-96x96.png" sizes="96x96">
-  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/android-chrome-192x192.png" sizes="192x192">
-  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-16x16.png" sizes="16x16">
-  <link rel="manifest" href="<?php bloginfo('template_directory'); ?>/images/favicons/manifest.json">
-  <link rel="mask-icon" href="<?php bloginfo('template_directory'); ?>/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
-  <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="msapplication-TileImage" content="<?php bloginfo('template_directory'); ?>/images/favicons/mstile-144x144.png">
-  <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/images/favicons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-194x194.png" sizes="194x194">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="<?php bloginfo('template_directory'); ?>/images/favicons/manifest.json">
+    <link rel="mask-icon" href="<?php bloginfo('template_directory'); ?>/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-TileImage" content="<?php bloginfo('template_directory'); ?>/images/favicons/mstile-144x144.png">
+    <meta name="theme-color" content="#ffffff">
   <!--/FAVICONS-->
 </head>
 <body>
   <?php include(TEMPLATEPATH.'/warning.php'); ?>
-
 
 	<div class="wrapper <?php if (is_front_page()) { echo 'wrapper--frontPage'; }?>">
 
@@ -77,4 +76,4 @@
           <a href="http://www.boardpusher.com/riddickart" target="_blank">skateboards</a>
         </h5>
       </div>
-    </div> <!--/header-->
+    </div> <!--/R__header-->
